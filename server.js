8e02c4b8e9f543addf525235179523947c95f927
Isaias Palaço <isaiaspalaco@gmail.com>
2026-06-02 00:55:22 +0200

    feat: MVC pattern

--- server.js
+++ server.js
@@ -2,13 +2,11 @@ import express from 'express';
 import { fileURLToPath } from 'url';
 import path from 'path';
 import { testConnection } from './src/models/db.js';
-import { getAllOrganizations } from './src/models/organizations.js';
-import { getAllProjects } from './src/models/projects.js';
-import { getAllCategories } from './src/models/categories.js';
+import router from './src/routes.js';
 
-
-// Define the the application environment
+// Define the application environment
 const NODE_ENV = process.env.NODE_ENV?.toLowerCase() || 'production';
+
 // Define the port number the server will listen on
 const PORT = process.env.PORT || 3000;
 
@@ -17,6 +15,9 @@ const __dirname = path.dirname(__filename);
 
 const app = express();
 
+/**
+  * Configure Express middleware
+  */
 
 // Serve static files from the public directory
 app.use(express.static(path.join(__dirname, 'public')));
@@ -26,13 +27,6 @@ app.set('view engine', 'ejs');
 
 // Tell Express where to find your templates
 app.set('views', path.join(__dirname, 'src/views'));
-
-// Test route for 500 errors
-app.get('/test-error', (req, res, next) => {
-    const err = new Error('This is a test error');
-    err.status = 500;
-    next(err);
-});
 
 // Middleware to log all incoming requests
 app.use((req, res, next) => {
@@ -48,37 +42,8 @@ app.use((req, res, next) => {
     next();
 });
 
-/**
- * Routes
- */
-app.get('/', async (req, res) => {
-    const title = 'Home';
-    res.render('home', { title });
-});
-
-app.get('/organizations', async (req, res) => {
-    const organizations = await getAllOrganizations();
-    const title = 'Our Partner Organizations';
-
-    res.render('organizations', { title, organizations });
-});
-
-
-app.get('/projects', async (req, res) => {
-    try {
-        const projects = await getAllProjects(); // Fetch the joined data
-        const title = 'Service Projects';
-        res.render('projects', { title, projects });
-    } catch (error) {
-        console.error('Error fetching projects:', error);
-        res.status(500).send('Internal Server Error');
-    }
-});
-
-app.get('/categories', async (req, res) => {
-    const categories = await getAllCategories();
-    res.render('categories', { title: 'Project Categories', categories });
-});
+// Use the imported router to handle routes
+app.use(router);
 
 // Catch-all route for 404 errors
 app.use((req, res, next) => {
